f861f1f15a12f96348178ffc0716e4ae0bde683e
Ashish-wel <ashishnagda34@gmail.com>
2025-08-22 22:42:14 +0530

    Create task 2.py

---
+++ task 2.py
@@ -0,0 +1,12 @@
+numbers = list(range(1,11))
+first_five = numbers[:5]
+reversed_list = first_five[::-1]
+
+print("Original list:", numbers)
+print("Extracted first five elements:", first_five)
+print("Reversed Extracted elements:", reversed_list)
+
+=== Output ===
+Original list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Extracted first five elements: [1, 2, 3, 4, 5]
+Reversed extracted elements: [5, 4, 3, 2, 1]
